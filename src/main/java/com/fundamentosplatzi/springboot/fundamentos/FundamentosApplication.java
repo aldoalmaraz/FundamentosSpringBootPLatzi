d524dcea4b342a078f14681d8ed7978ea5b65f9a
doalfull <doalfull@gmail.com>
2022-04-19 22:41:15 -0500

    Agregando configuracion properties y clases para conectar a la base de datos

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,11 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,21 +16,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
-
+private final Log LOGGER= LogFactory.getLog(FundamentosApplication.class);
 
 	// En esta clase se realiza la inyeccion de dependecias.
 	private ComponentDependency componentDependency; // agregamos la interfaz del componente o bean
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
+
 
 
 	//por medio del constructor agregamos de igual maneta la interfaz.
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency ,MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency ,MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency=componentDependency;
 		this.myBean=myBean;
 		this.myBeanWithDependency=myBeanWithDependency;
 		this.myBeanWithProperties=myBeanWithProperties;
+		this.userPojo=userPojo;
+
 	}
 
 	public static void main(String[] args) {
@@ -38,5 +47,12 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.Function());
+		System.out.println(userPojo.getEmail()+" - "+userPojo.getPassword());
+		try{
+int value=10/0;
+LOGGER.debug( "Mi valor " + value);
+		}catch (Exception e){
+		LOGGER.error("Esto es un error al dividir por 0!!"+  e.getMessage());
+		}
 	}
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -2,10 +2,16 @@ package com.fundamentosplatzi.springboot.fundamentos.configuration;
 
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
+@EnableConfigurationProperties(UserPojo.class)
 @Configuration
 
 //con @value te traes la variable que creaste en la clase de configuracion application propierties
@@ -23,4 +29,21 @@ private String random;
     public MyBeanWithProperties function(){
     return new MyBeanWithPropertiesImplement(name,apellido);
 }
+
+//IMPLEMENTACION DE UNA NUEVA BASE DE DATOS.
+
+    @Bean
+    public DataSource datasource(){
+        DataSourceBuilder dataSourceBuilder= DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+
+
+
 }
+
+
